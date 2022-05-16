a69b6ee1d5e4c6525cc590129c1a28ec3f2d2c75
lisardy <103500239+lisardy@users.noreply.github.com>
2022-05-16 21:00:16 +0100

    Added initialiser for values
    Made it so that Values has a pre-loaded class to cache values

--- data/values.py
+++ data/values.py
@@ -1,8 +1,29 @@
-def get_X_values():
-    pass
+PARAMETERS_FILE = "parameters.txt"
+CHANNELS_FILE = "channels.txt"
 
-def get_m():
-    pass
 
-def get_c():
-    pass
+class Values():
+    X_values = None
+    m_value = None
+    c_value = None
+
+    def __init__(self):
+        _load_values()
+
+    def _load_values(self):
+        pass
+
+    def _load_parameters(self):
+        pass
+
+    def _load_channels(self):
+        pass
+
+    def get_X_values(self):
+        return self.X_values
+
+    def get_m(self):
+        return self.m_value
+
+    def get_c(self):
+        return self.c_value
