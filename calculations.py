68ebc45c2e18f40146d502c97a50bcb641dc8f5d
lisardy <103500239+lisardy@users.noreply.github.com>
2022-05-16 20:52:27 +0100

    Added main files
    Main files added to project

---
+++ calculations.py
@@ -0,0 +1,24 @@
+def calculate_Y():
+    pass
+
+def calculate_B():
+    pass
+
+def calculate_b():
+    pass
+
+def calculate_A():
+    pass
+
+def calculate_C():
+    pass
+
+#Function 1:
+#Y=mX+c
+#Function 2:
+#B=A+Y
+#b=mean(B)
+#Function 3:
+#A=1X
+#Function 4:
+#C=X+b
---
+++ data/values.py
@@ -0,0 +1,8 @@
+def get_X_values():
+    pass
+
+def get_m():
+    pass
+
+def get_c():
+    pass
